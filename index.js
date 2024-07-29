2d282a3edb4397d830c301d2c0be0284ff9bdd14
chananajatin <jatinchanana001@gmail.com>
2024-07-29 22:15:35 +0530

    optimisation an cleanup

--- index.js
+++ index.js
@@ -2,23 +2,43 @@ const express = require('express');
 const app = express();
 const ytdl = require('ytdl-core');
 const cors = require('cors');
+const youtubedl = require('youtube-dl-exec');
 
 app.use(cors());
 
+const getAudio = (videoId) =>
+  new Promise((res, rej) => {
+    youtubedl(`https://www.youtube.com/watch?v=${videoId}`, {
+      dumpSingleJson: true,
+      noCheckCertificates: true,
+      noWarnings: true,
+      preferFreeFormats: true,
+      addHeader: ['referer:youtube.com', 'user-agent:googlebot'],
+    }).then((output) => {
+      const audioOutput = output.requested_formats?.filter(
+        (format) => format.video_ext === 'none' && format.aspect_ratio === null
+      );
+      res(audioOutput[0]);
+    });
+  });
+
+const map = {};
+
 app.get('/song/:id', async (req, res) => {
   try {
-    let info = await ytdl.getInfo(req.params.id);
-    let audioFormatHigh = ytdl.chooseFormat(info.formats, {
-      quality: 'highest',
-      filter: 'audioonly',
-    });
-    let audioFormatLow = ytdl.chooseFormat(info.formats, {
-      quality: 'lowest',
-      filter: 'audioonly',
-    });
+    const videoId = req.params.id;
+    if (map[videoId]) {
+      res.status(200).json({
+        audioUrl: map[videoId],
+      });
+    }
+    let audioFormat = await getAudio(videoId);
+    if (!audioFormat?.url) {
+      throw new Error();
+    }
+    map[videoId] = audioFormat?.url;
     res.status(200).json({
-      audioFormatHigh: audioFormatHigh.url,
-      audioFormatLow: audioFormatLow.url,
+      audioUrl: audioFormat?.url || '',
     });
   } catch (err) {
     // console.error(error);
